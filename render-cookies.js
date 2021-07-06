0eee06729ea4495c39ab34bbe246c5d1f633768c
Triana Cerda <trianaRcerda@gmail.com>
2021-07-06 15:02:02 -0700

    added functions to be exported

--- render-cookies.js
+++ render-cookies.js
@@ -1,1 +1,25 @@
-//write functions here: const
+export function renderCookies(cookies){
+
+    const cookiesLi = document.createElement ('li'); 
+    const cookiesh3 = document.createElement('h3'); //name
+    cookiesh3.textContent = cookies.name;
+    
+    const cookiesImg = document.createElement ('img'); //img
+    cookiesImg.src = `./assets/${cookies.img}`;
+    cookiesImg.alt = cookies.name;
+
+    const cookiesSpan = document.createElement ('span'); //price
+    cookiesSpan.textContent = `${cookies.price}`;
+
+    const cookiesBtn = document.createElement('button'); //button
+    cookiesBtn.textContent = 'Add to Jar';
+
+
+    cookiesLi.appendChild(cookiesh3);
+    cookiesLi.appendChild(cookiesImg);
+    cookiesLi.appendChild(cookiesSpan);
+    cookiesLi.appendChild(cookiesBtn);
+    
+    
+    return cookiesLi;
+}
