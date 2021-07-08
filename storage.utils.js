719c1479c137301f439bfbe0d4d3b44f65702af0
Triana Cerda <trianaRcerda@gmail.com>
2021-07-08 15:28:06 -0700

    working tests and functions

--- storage.utils.js
+++ storage.utils.js
@@ -1,10 +1,10 @@
 import { findById } from './utils.js';
 
 //functions for storage.tests.js 
-let CART = 'cookie-cart';
+export let CART = 'cookie-cart';
 
 export function getCartLs(){
-    let stringCart = localStorage.get(CART) || '[]';
+    let stringCart = localStorage.getItem(CART) || '[]';
     const cart = JSON.parse(stringCart);
     return cart;
 }
@@ -14,9 +14,16 @@ export function addItemtoCartById(cookieId){
     const cookie = findById(cart, cookieId);
 
     if (cookie) {
-        cookie.qty ++;
+        cookie.qty += 1;
     } else {
         const newCookie = { id: cookieId, qty: 1 };
         cart.push(newCookie);
     }
+    localStorage.setItem(CART, JSON.stringify(cart));
 }
+
+export function clearCart(){
+    localStorage.removeItem(CART);
+}
+
+
